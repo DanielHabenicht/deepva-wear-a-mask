559f3e42785b83f6d91e14ff02d42e2c2adec4fc
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-09-01 19:27:51 +0200

    first working prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { WebcamImage, WebcamInitError } from 'ngx-webcam';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('openClose', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('100ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
-export class AppComponent {
-  title = 'wear-a-mask';
+export class AppComponent implements OnInit {
+  detectionState: DetectionState = DetectionState.neutral;
+  detectionStateEnum: typeof DetectionState = DetectionState;
+  public innerWidth: number = 0;
+  public innerHeight: number = 0;
+  public captureImageObservable: Observable<any> = new Observable<any>(
+    (observer) => {
+      this.captureImageTrigger = observer;
+    }
+  );
+  private captureImageTrigger: Observer<any> | undefined;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+  }
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+    this.capture();
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.detectionState = DetectionState.cameraDeactivated;
+    if (
+      error.mediaStreamError &&
+      error.mediaStreamError.name === 'NotAllowedError'
+    ) {
+      console.warn('Camera access was not allowed by user!');
+    }
+  }
+
+  capture() {
+    setTimeout(() => {
+      this.captureImageTrigger?.next('');
+      this.capture();
+    }, 1000);
+  }
+
+  capturedImage(event: WebcamImage) {
+    console.log(event);
+  }
+}
+
+export enum DetectionState {
+  cameraDeactivated,
+  neutral,
+  hasMask,
+  noMask,
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WebcamModule } from 'ngx-webcam';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    WebcamModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
