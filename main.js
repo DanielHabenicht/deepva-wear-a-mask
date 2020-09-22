9172ead2a9c428fb5c78651499d29b200cd70769
DanielHabenicht <daniel-habenicht@outlook.de>
2020-09-22 07:56:04 +0000

    Deploying to gh-pages from  @ 59919bb057df80f7f4f65e5b9d952ef1164c4127 🚀

--- main.js
+++ main.js
@@ -242,7 +242,7 @@ class AppComponent {
     }
     analyzeImage(image) {
         this.httpClient
-            .post('/mask', {
+            .post(src_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].baseUrl + '/mask', {
             image: image,
         })
             .subscribe((result) => {
@@ -636,20 +636,13 @@ AppModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineInjector
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 const environment = {
-    production: false
+    /**
+     * Base Url without trailing slash
+     */
+    baseUrl: 'http://localhost:4200',
+    production: 'true'
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
 
 
 /***/ }),
